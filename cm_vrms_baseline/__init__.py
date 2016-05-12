652d927daae6ddea6f4dee514f8b467ea36b5e9c
yezitt <yezitt@163.com>
2016-05-12 18:17:39 +0800

    eclipse commit

---
+++ cm_vrms_baseline/__init__.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+default_app_config = 'cm_vrms_baseline.apps.CMVRMSConfig'
+app_title = u'版本基线信息'
---
+++ cm_vrms_wiki/__init__.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+default_app_config = 'cm_vrms_wiki.apps.CMVRMSConfig'
+app_title = u'配置项管理系统'
